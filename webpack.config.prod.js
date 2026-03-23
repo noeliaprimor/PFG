b209a1572d9106b81bb66983a393d99ebe569b68
Noelia Prieto <noelia.prieto@cobee.io>
2026-03-23 09:05:47 +0100

    Add Cloudflare Pages deployment workflow and related documentation

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,21 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-    }),
     new CopyPlugin({
       patterns: [
+        { from: 'index.html', to: 'index.html' },
+        { from: 'modulo1', to: 'modulo1' },
+        { from: 'modulo2', to: 'modulo2' },
+        { from: 'modulo3', to: 'modulo3' },
+        { from: 'partials', to: 'partials' },
+        { from: 'data', to: 'data' },
         { from: 'img', to: 'img' },
         { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
+        { from: 'js', to: 'js' },
         { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
